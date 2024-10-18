4831f225ba7941e0fb481b8fdf52eb45d551326a
dudeperf3ct <dudeperf3ct@users.noreply.github.com>
2024-10-18 15:52:55 +0530

    Add torch-tensorrt experiment

---
+++ torch_trt/README.md
@@ -0,0 +1,146 @@
+# Torch-TensorRT
+
+There are 3 backends supported by benchmark script
+
+* PyTorch
+* TorchScript
+* TensorRT
+
+The latency and throughput for all the backends are stored in json file under `model_name` folder.
+
+For `tensorrt` backend we also store the profiling traces under the `model_name/profiling_{datetimestamp}` folder. We calculate layer-wise latency for `runs` (total number of runs to benchmark) iterations and save the trace every `profiler-iter` iterations to the disk.
+
+> Note: The profiling trace for layer-wise latency of TensorRT models can be viewed at <https://ui.perfetto.dev/>.
+
+## Getting Started
+
+There are two approaches to run this project.
+
+### Locally
+
+> Note: Building and install `torch-tensort` package takes about 15 mins.
+
+```bash
+uv venv -- python 3.11
+source .venv/bin/activate
+uv pip install -r pyproject.toml
+```
+
+### Run the benchmark script
+
+For example let us run the benchmark script using Mobilenetv2 model from [pytorch hub](https://pytorch.org/hub/pytorch_vision_mobilenet_v2/)
+
+```bash
+uv run benchmark.py --model mobilenet_v2 --backend pytorch --save-result
+uv run benchmark.py --model mobilenet_v2 --backend torchscript --save-result
+uv run benchmark.py --model mobilenet_v2 --backend tensorrt --save-result
+```
+
+### Visualize TensorRT profiler
+
+```bash
+# By default alexnet model is used
+uv run viz_profiler.py
+
+# Mobilenetv2 model 
+uv run viz_profiler.py --profile-dir results/mobilenet_v2/profiling_20241018-123805
+
+# Resnet18 model 
+uv run viz_profiler.py --profile-dir results/resnet18/profiling_20241018-123926
+```
+
+An example output is shown below. The table shows following information for TensorRT model
+
+* Layer Name
+* Input Dimension
+* Output Dimension
+* Average Latency for layer (seconds)
+
+![Layerwise Latency](./assets/layer_wise_latency.png)
+
+There are boxplots for various model under the `results/model_name` directory. The boxplot plots layer-wise latency for each layer of the TensorRT model.
+
+### Visualize latency and throughput across backends
+
+```bash
+# By default alexnet model is used
+uv run viz.py
+
+# Mobilenetv2 model 
+uv run viz.py --model-dir results/mobilenet_v2
+
+# Resnet18 model 
+uv run viz.py --model-dir results/resnet18
+```
+
+The comparison plots for each model are saved nder the `results/model_name` directory. An example of `resnet18` model shown below.
+
+![Latency and Throughput](./results/resnet18/latency_throughput.png)
+
+### Docker
+
+> Note: Size of Docker image `nvcr.io/nvidia/pytorch:24.09-py3` is around 9 GB.
+
+```bash
+docker run --gpus all -it --rm -v $(pwd)/:/workspace/  nvcr.io/nvidia/pytorch:24.09-py3
+```
+
+Inside docker container run the benchmark script using following command,
+
+```bash
+cd torch_trt
+python benchmark.py --model mobilenet_v2 --backend pytorch --save-result
+```
+
+## Notes and Limitations
+
+Exploring and reading [Torch-Tensorrt documentation](https://pytorch.org/TensorRT/), I found that there are 3 approaches to converting a torch model to TensorRT.
+
+* Dynamo Front-end
+* TorchScript Front-end
+* FX Front-end
+
+It is not particularly clear which approach to prefer for a new user. For this experiment, I used `Dyanmo` approach that uses a FX Graph Module to create a TensorRT module.
+
+There were few notes/limitations to this library.
+
+* Dynamo approach does not provide `INT8` support at the moment.
+
+* `use_python_runtime` parameter to the compiler changes which profiler is being used.
+  * If set to `True`, it uses [PythonTorchTensorRTModule](https://github.com/pytorch/TensorRT/blob/d11ff5c14cb45c975b4a9698b211ebacf1a36bb7/py/torch_tensorrt/dynamo/runtime/_PythonTorchTensorRTModule.py#L26C7-L26C32) as it's runtime. This approach _does not_ provide an option in it's [enable_profiling](https://github.com/pytorch/TensorRT/blob/d11ff5c14cb45c975b4a9698b211ebacf1a36bb7/py/torch_tensorrt/dynamo/runtime/_PythonTorchTensorRTModule.py#L417) function to save the layer-wise latency. It instead just prints the traces on the stdout.
+  * If set to `False`, it uses [TorchTensorRTModule](https://github.com/pytorch/TensorRT/blob/d11ff5c14cb45c975b4a9698b211ebacf1a36bb7/py/torch_tensorrt/dynamo/runtime/_TorchTensorRTModule.py#L53) as it's runtime. This approach _does_ provide [option](https://github.com/pytorch/TensorRT/blob/d11ff5c14cb45c975b4a9698b211ebacf1a36bb7/py/torch_tensorrt/dynamo/runtime/_TorchTensorRTModule.py#L283) to store the profiling traces in a directory.
+
+* At present there's no clean way to profile as noted in one of the issues on the TensorRT repo : <https://github.com/pytorch/TensorRT/issues/1467>. We use a hacky approach to enable profiling as suggested in the comment on the issue.
+
+* There are different modes of [ProfilingVerbosity](https://docs.nvidia.com/deeplearning/tensorrt/developer-guide/index.html#engine-inspector) that shows how detailed information for a particular layer is recorded. Apparently for `dynamo` approach, this is configured implicitly via `debug` parameter [here](https://github.com/pytorch/TensorRT/blob/d11ff5c14cb45c975b4a9698b211ebacf1a36bb7/py/torch_tensorrt/dynamo/conversion/_TRTInterpreter.py#L214).
+
+* There are various parameter like `min_block_size` and `optimization` that affect the TensorRT run times.
+
+* For example, to see if all operators are supported, we set `min_block_size` value to some higher value `100`. This shows how many graph fragmentation occurs and what is recommended value.
+
+    Here's an example output from logs for the `resnet18` model.
+
+    ```bash
+    Graph Structure:
+
+    Inputs: List[Tensor: (1, 3, 224, 224)@float16]
+        ...
+        TRT Engine #1 - Submodule name: _run_on_acc_0
+        Engine Inputs: List[Tensor: (1, 3, 224, 224)@float16]
+        Number of Operators in Engine: 89
+        Engine Outputs: Tensor: (1, 1000)@float16
+        ...
+    Outputs: List[Tensor: (1, 1000)@float16]
+
+    ------------------------- Aggregate Stats -------------------------
+
+    Average Number of Operators per TRT Engine: 89.0
+    Most Operators in a TRT Engine: 89
+
+    ********** Recommendations **********
+
+    - For minimal graph segmentation, select min_block_size=89 which would generate 1 TRT engine(s)
+    - The current level of graph segmentation is equivalent to selecting min_block_size=89 which generates 1 TRT engine(s)
+    ```
+
+* `optimization-level` parameter decides how long to spend searching for an optimal path to minimize run time for a build. It's value ranges from 0 to 5 (higher levels imply longer build time).
---
+++ torch_trt/viz_profiler.py
@@ -0,0 +1,211 @@
+"""Visualizes layer wise latency from profiler tracing."""
+
+import glob
+from dataclasses import dataclass
+from typing import Any
+import json
+from rich.console import Console
+from rich.table import Table
+import argparse
+from rich.markup import escape
+import numpy as np
+from tqdm import tqdm
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+
+
+@dataclass
+class LayerInfo:
+    layer_name: str
+    layer_type: str
+    input_dimension: list[int]
+    output_dimension: list[int]
+    latencies: list[list[float]]
+
+
+def read_trace_file(file_path: str) -> Any:
+    """Parse trace file.
+
+    Args:
+        file_path: Path to trace file.
+
+    Returns:
+        Extracted data.
+    """
+    with open(file_path, "r") as fp:
+        data = json.load(fp)
+    return data
+
+
+def load_trace_data(profile_dir: str) -> dict[str, LayerInfo]:
+    """Extract latencies for each layer across multiple files.
+
+    Args:
+        profile_dir: Path to profiler and trace data
+
+    Returns:
+        Dictionary mapping layer of name and
+        corresponding latencies for the layer
+    """
+    layer_data: dict[str, LayerInfo] = {}
+
+    trace_file_paths = (
+        f"{profile_dir}/*/_run_on_acc_0_engine_engine_exectuion_profile.trace"
+    )
+    for trace_file in tqdm(glob.glob(trace_file_paths), desc="Processing Trace Files"):
+        data = read_trace_file(trace_file)
+
+        for layer in data:
+            layer_name = layer["name"]
+            duration = round(layer["dur"] / 1e3, 3)
+            if layer_name not in layer_data:
+                layer_data[layer_name] = LayerInfo(
+                    layer_name=layer_name,
+                    layer_type="",
+                    input_dimension=[],
+                    output_dimension=[],
+                    latencies=[],
+                )
+            layer_data[layer_name].latencies.append(duration)
+    return layer_data
+
+
+def load_layer_metadata(
+    profile_dir: str, layer_data: dict[str, LayerInfo]
+) -> list[LayerInfo]:
+    """Extract layer related information.
+
+    Args:
+        profile_dir: Path to profiler and trace data
+        layer_data: Dictionary mapping layer of name and
+            corresponding latencies for the layer
+
+    Returns:
+        List of all metadata related to layer
+    """
+    layer_file_paths = f"{profile_dir}/*/_run_on_acc_0_engine_layer_information.json"
+    layer_file_path = glob.glob(layer_file_paths)[0]
+    layer_info_data = read_trace_file(layer_file_path)
+
+    for layer in layer_info_data["Layers"]:
+        layer_name = layer["Name"]
+        if layer_name in layer_data:
+            layer_data[layer_name].layer_type = layer["LayerType"]
+            layer_data[layer_name].input_dimension = layer["Inputs"][0]["Dimensions"]
+            layer_data[layer_name].output_dimension = layer["Outputs"][0]["Dimensions"]
+    return list(layer_data.values())
+
+
+def load_layer_data(profile_dir: str) -> list[LayerInfo]:
+    """Extract layer and it's metadata from profiler data.
+
+    Args:
+        profile_dir: Path to profiler and trace data
+
+    Returns:
+        List of all metadata related to layer
+    """
+    layer_data = load_trace_data(profile_dir)
+    return load_layer_metadata(profile_dir, layer_data)
+
+
+def create_dataframe(layer_infos: list[LayerInfo]) -> pd.DataFrame:
+    """Create a dataframe consisting of only layers and its latencies.
+
+    Args:
+        layer_infos: List of all metadata related to layer
+
+    Returns:
+        Dataframe containing layer and latency
+    """
+    data = {"Layer Name": [], "Latency": []}
+    for layer in layer_infos:
+        data["Layer Name"].extend([layer.layer_name] * len(layer.latencies))
+        data["Latency"].extend(layer.latencies)
+    df = pd.DataFrame(data)
+    return df
+
+
+def plot_layer_name_latencies(layer_infos: list[LayerInfo], save_fig_dir: str) -> None:
+    """Plot a boxplot for layer-wise latencies and save it to a file.
+
+    Args:
+        layer_infos: List of all metadata related to layer
+        save_fig_dir: Directory to save figure.
+    """
+    save_path = os.path.join(save_fig_dir, "layer_latencies_boxplot.png")
+    df = create_dataframe(layer_infos)
+    plt.figure(figsize=(25, 25))
+    sns.boxplot(x="Layer Name", y="Latency", data=df)
+    plt.xticks(rotation=90, ha="right")
+    plt.xlabel("Layer Name")
+    plt.ylabel("Latency (sec)")
+    plt.title("Layer-wise Latencies")
+    plt.savefig(save_path, dpi=300)
+    print(f"Saved boxplot to {save_path}")
+
+    plt.show()
+
+
+def layer_information(model_name: str, layer_infos: list[LayerInfo]) -> Table:
+    """Create a rich table to display layer related information.
+
+    Args:
+        model_name: Name of the model
+        layer_infos: List of all metadata related to layer
+
+    Returns:
+        Table containing layer name, type, input and output dimension
+        and average layer-wise latency
+    """
+    table = Table(title=f"Layer wise Latency for {model_name} model", show_lines=True)
+    table.add_column("Layer Name", justify="left", style="magenta")
+    table.add_column("Layer Type", style="red")
+    table.add_column("Input Dimension", style="cyan")
+    table.add_column("Output Dimension", style="blue")
+    table.add_column("Average Latency (sec)", justify="right", style="green")
+
+    for layer in layer_infos:
+        table.add_row(
+            escape(layer.layer_name),
+            layer.layer_type,
+            str(layer.input_dimension),
+            str(layer.output_dimension),
+            str(round(np.mean(layer.latencies), 3)),
+        )
+    return table
+
+
+def main(args: argparse.Namespace) -> None:
+    """Main entrypoint.
+
+    Args:
+        args: Arguments from CLI
+    """
+    console = Console()
+    model_name = str(args.profile_dir.split("/")[1]).capitalize()
+    save_fig_dir = "/".join(args.profile_dir.split("/")[:2])
+
+    # Show layer information and latency
+    layer_infos = load_layer_data(args.profile_dir)
+    table = layer_information(model_name, layer_infos)
+    console.print(table)
+
+    plot_layer_name_latencies(layer_infos, save_fig_dir)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="Visualize tensorrt profiler trace",
+    )
+    parser.add_argument(
+        "--profile-dir",
+        type=str,
+        default="results/alexnet/profiling_20241018-123649",
+        help="Specify name of profiler folder for the model",
+    )
+
+    args = parser.parse_args()
+    main(args)
