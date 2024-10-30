f6950ac6a754f8a59b37f70487dd7f24c3af5de9
Oscar <25309418+osw282@users.noreply.github.com>
2024-10-30 08:59:01 +0000

    [SD-56] Implement the inference process (#4)
    * Power logging dir
    
    * Power log draft
    
    * Update logger
    
    * Add top level doc string
    
    * Test logging script on Jetson
    
    * Update readme
    
    * Update based on review comments
    
    * Update based on review comments
    
    * Merge SD-55
    
    * Test inference layer time
    
    * Commit unsave changes
    
    * Log power and layer wise latency with time stamp
    
    * Add missing dependency
    
    * Remove dependency already came with containers
    
    * Add missing dep
    
    * Add init
    
    * Update from testing on Jetson
    
    * Increase default number of runs
    
    * Update script to read current and voltage
    
    * Add gitignore
    
    * Add measuring idling power script
    
    * Update readme
    
    * Update readme and print statment
    
    * Update idling power script
    
    * Move print device statment up
    
    * Update readme
    
    * Update based on review comment
    
    * Update based on review comments
    
    * Update readme
    
    * Update Readme
    
    * Remove old power measuring script
    
    * Update idle duration arg
    
    * Add results
    
    * Add log file
    
    * Update file name output to be more descriptive
    
    * Rename log file
    
    * Include system config in readme
    
    * Update Readme

---
+++ jetson/power_logging/measure_idling_power.py
@@ -0,0 +1,90 @@
+"""A script to measure the power used when the device is idling.
+
+Before running this script, make sure that the Jetson has been set up max setting.
+The same setting that will be used for inference.
+
+To maximize Jetson Orin performance and fan speed:
+    sudo /usr/bin/jetson_clocks --fan
+"""
+import time
+from datetime import datetime
+from pathlib import Path
+import argparse
+from rich import print
+
+
+def power_logging(args: argparse.Namespace) -> list[tuple]:
+    """
+    Measure voltage and current from the system file for a duration of 1 minute.
+
+    Args:
+        args: Command-line arguments, including the output directory for storing the log file
+
+    Returns:
+        A list of instantaneous voltage and current readings captured during the measurement period.
+    """
+    logs = []
+    start_time = time.time()  # Record the start time
+
+    print(f"Logging idling power usage for {args.idle_duration} seconds ...")
+    while (time.time() - start_time) < args.idle_duration:  # Run for 1 minute
+        # Read voltage and current from sys file
+        with open("/sys/bus/i2c/drivers/ina3221/1-0040/hwmon/hwmon1/in1_input", "r") as voltage:
+            mV = float(voltage.read())
+        with open("/sys/bus/i2c/drivers/ina3221/1-0040/hwmon/hwmon1/curr1_input", "r") as current:
+            mC = float(current.read())
+
+        logs.append((mV, mC))
+    print("Power logging complete.")
+
+    return logs
+
+
+def compute_average_idling_power(args: argparse.Namespace, logs: list[tuple]) -> None:
+    """
+    Compute and log the average idling power based on voltage and current readings.
+
+    Args:
+        args: Command-line arguments, including the output directory for storing the log file.
+        logs: A list of tuples containing instantaneous voltage (mV) and current (mA) readings
+              recorded during the measurement period.
+
+    Writes:
+        A log file in the specified output directory containing the computed average idling power in mW.
+    """
+    print(f"Computing average idling power.")
+    mWs = [mV * mC for mV, mC in logs]
+    average_mW = sum(mWs) / len(mWs)
+    print(f"Average idling power: {average_mW} mW")
+
+    # Ensure the results directory exists
+    Path(args.result_dir).mkdir(exist_ok=True, parents=True)
+
+    current_dt = datetime.now().strftime("%Y%m%d-%H%M%S")
+    with open(f"{args.result_dir}/{args.idle_duration}_seconds_idling_power_log_{current_dt}.log", "w") as f:
+        f.writelines(f"The average idling power measured: {average_mW} mW")
+
+    print(f"Log file created at {args.result_dir}/{args.idle_duration}_seconds_idling_power_log_{current_dt}.log")
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="Power Logging For Idling",
+        description="Collect power usage data when Jetson is idling."
+    )
+    parser.add_argument(
+        "--idle-duration",
+        type=int,
+        default=60,
+        help="Duration (in seconds) to measure power usage during idle state. Default is 60 seconds."
+    )
+    parser.add_argument(
+        "--result-dir",
+        type=str,
+        default="results",
+        help="The directory to save the log result."
+    )
+    args = parser.parse_args()
+    logs = power_logging(args)
+    compute_average_idling_power(args, logs)
---
+++ jetson/power_logging/measure_inference_power.py
@@ -0,0 +1,128 @@
+"""Script for collecting power usage during the inference cycle of a given CNN model.."""
+
+from multiprocessing import Process, Event
+from multiprocessing.synchronize import Event as EventClass
+import argparse
+from pathlib import Path
+from datetime import datetime
+from model.benchmark import benchmark
+import multiprocessing
+
+multiprocessing.set_start_method('spawn', force=True)
+
+
+def power_logging(event: EventClass, args: argparse.Namespace) -> None:
+    """
+    Read voltage, current and power from sys file.
+
+    Args:
+        event: An object that manages a flag for communication among processes.
+        args: Arguments from CLI.
+    """
+    Path(args.result_dir).mkdir(exist_ok=True, parents=True)
+
+    logs = []
+
+    while not event.is_set():
+        with open("/sys/bus/i2c/drivers/ina3221/1-0040/hwmon/hwmon1/in1_input", "r") as voltage:
+            mV = float(voltage.read())
+        with open("/sys/bus/i2c/drivers/ina3221/1-0040/hwmon/hwmon1/curr1_input", "r") as current:
+            mC = float(current.read())
+
+        current_time = datetime.now().strftime("%Y%m%d-%H:%M:%S.%f")  # Time with seconds and microseconds
+        logs.append(f"{current_time},{mV},{mC}\n")  # Log the time, voltage and current.
+
+    current_dt = datetime.now().strftime("%Y%m%d-%H%M%S")
+    with open(f"{args.result_dir}/{args.runs}_cycles_power_log_{current_dt}.log", "w") as f:
+        f.writelines(logs)
+
+
+def inference(
+    event: EventClass,
+    args: argparse.Namespace,
+) -> None:
+    """
+    Call the benchmark function to start inferencing cycles.
+
+    Save layer wise latency with time stamp.
+
+    Args:
+        event: An object that manages a flag for communication among processes.
+        args: _Arguments from CLI.
+    """
+    benchmark(args)
+    event.set()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="Power Logging for CNN Inference Cycle",
+        description="Collect power usage data during inference cycles for ImageNet pretrained CNN models."
+    )
+    parser.add_argument(
+        "--model",
+        type=str,
+        default="mobilenet_v2",
+        help="Specify name of pretrained CNN model from PyTorch Hub."
+        "For more information on PyTorch Hub visit: "
+        "https://pytorch.org/hub/research-models",
+    )
+    parser.add_argument(
+        "--dtype",
+        type=str,
+        default="float16",
+        choices=["float16", "bfloat16", "float32"],
+        help="Data type for model weights and activations.\n\n"
+        '* "float16" is the same as "half".\n'
+        '* "bfloat16" for a balance between precision and range.\n'
+        '* "float32" for FP32 precision.',
+    )
+    parser.add_argument(
+        "--input-shape",
+        type=int,
+        nargs="+",
+        default=[1, 3, 224, 224],
+        help="Input shape BCHW",
+    )
+    parser.add_argument(
+        "--warmup",
+        type=int,
+        default=50,
+        help="Number of iterations to perform warmup before benchmarking",
+    )
+    parser.add_argument(
+        "--runs",
+        type=int,
+        default=30000,
+        help="Number of inference cycle to run"
+    )
+    parser.add_argument(
+        "--optimization-level",
+        type=int,
+        default=5,
+        help="Builder optimization 0-5, higher levels imply longer build time, "
+        "searching for more optimization options.",
+    )
+    parser.add_argument(
+        "--min-block-size",
+        type=int,
+        default=5,
+        help="Minimum number of operators per TRT-Engine Block",
+    )
+    parser.add_argument(
+        "--result-dir",
+        type=str,
+        default="results",
+        help="The directory to save the log result."
+    )
+    args = parser.parse_args()
+
+    event = Event()
+    power_logging_process = Process(target=power_logging, args=(event, args))
+    power_logging_process.start()
+
+    inference_process = Process(target=inference, args=(event, args))
+    inference_process.start()
+
+    power_logging_process.join()
+    inference_process.join()
