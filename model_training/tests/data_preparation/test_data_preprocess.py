c65f8ce0e49dd3838dc93025b507ad370a1c34de
dudeperf3ct <dudeperf3ct@users.noreply.github.com>
2024-12-12 19:37:25 +0530

    Fix test data path

--- model_training/tests/data_preparation/test_data_preprocess.py
+++ model_training/tests/data_preparation/test_data_preprocess.py
@@ -1,4 +1,5 @@
 """Test DataPreprocessor class."""
+
 import json
 from pathlib import Path
 
@@ -12,30 +13,36 @@ from data_preparation.io_utils import parse_model_dir
 def data_preprocessor(tmp_path: Path) -> DataPreprocessor:
     """DataPreprocessor instance fixture.
 
-    See `example_data/idling_power.json` file for the example idling power.
+    See `example_raw_data/idling_power.json` file for the example idling power.
     A temporary is used for the results stored, if any.
     """
-    idle_power_log_path = Path(__file__).parent / "example_data" / "idling_power.json"
+    idle_power_log_path = (
+        Path(__file__).parent / "example_raw_data" / "idling_power.json"
+    )
     return DataPreprocessor(idle_power_log_path, tmp_path)
 
 
 @pytest.mark.parametrize(
     ["model_name"],
     [
-        ["simple"], # single layer, single reading
-        ["multiple_readings"], # single layer, multiple readings
-        ["multiple_layers"], # multiple layers, gaps between, before and after
-    ]
+        ["simple"],  # single layer, single reading
+        ["multiple_readings"],  # single layer, multiple readings
+        ["multiple_layers"],  # multiple layers, gaps between, before and after
+    ],
 )
-def test_compute_layer_metrics_by_cycle(model_name: str, data_preprocessor: DataPreprocessor) -> None:
+def test_compute_layer_metrics_by_cycle(
+    model_name: str, data_preprocessor: DataPreprocessor
+) -> None:
     """Test compute_layer_metrics_by_cycle."""
-    model_dir = Path(__file__).parent / "example_data" / model_name
-    power_log_file, trt_layer_latency_file, trt_engine_info_file = (
-        parse_model_dir(model_dir)
+    model_dir = Path(__file__).parent / "example_raw_data" / model_name
+    power_log_file, trt_layer_latency_file, trt_engine_info_file = parse_model_dir(
+        model_dir
     )
 
     with open(model_dir / "expected_metrics_by_cycle.json", "r") as file:
         expected_metrics_by_cycle = json.load(file)
 
-    metrics_by_cycle = data_preprocessor.compute_layer_metrics_by_cycle(power_log_file, trt_layer_latency_file, trt_engine_info_file)
+    metrics_by_cycle = data_preprocessor.compute_layer_metrics_by_cycle(
+        power_log_file, trt_layer_latency_file, trt_engine_info_file
+    )
     assert metrics_by_cycle == expected_metrics_by_cycle
