37c3622b4b1c9e4af59c3dab2dd12adb91f4ca7a
Misha Iakovlev <misha@fuzzylabs.ai>
2025-01-03 12:44:22 +0000

    Use correct layer detection script

--- jetson/power_logging/run_experiment.sh
+++ jetson/power_logging/run_experiment.sh
@@ -25,7 +25,7 @@ sleep 120
 models=("resnet18")
 #models=("alexnet" "vgg11" "vgg13" "vgg16" "vgg19" "mobilenet_v2" "mobilenet_v3_small" "mobilenet_v3_large" "resnet18" "resnet34" "resnet50" "resnet101" "resnet152" "lenet" "resnext50_32x4d" "resnext101_32x8d" "resnext101_64x4d" "convnext_tiny" "convnext_small" "convnext_base")
 # Number of inference cycles
-RUNS=3
+RUNS=30000
 
 # Iterate through models and run measure_inference_power.py script
 for model in "${models[@]}"
@@ -48,7 +48,7 @@ do
     $INPUT_SHAPE
 
   layers_for_ablation=( $(python detect_layers_for_ablation.py --model "$model") )
-  for layer in "${layers[@]}"
+  for layer in "${layers_for_ablation[@]}"
   do
     echo python measure_inference_power.py \
     --model "$model" \
