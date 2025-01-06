61223eb2dab688d36b37230728acb3a2cdcfa03e
Misha Iakovlev <misha@fuzzylabs.ai>
2025-01-06 17:14:45 +0000

    Push zeros to cuda correctly?

--- jetson/power_logging/ablation/modules.py
+++ jetson/power_logging/ablation/modules.py
@@ -15,7 +15,7 @@ class AblatedModule(torch.nn.Module):
         return self.reshape(x, self.output_shape(x))
 
     def reshape(self, x: torch.Tensor, shape) -> torch.Tensor:
-        return torch.zeros(shape).to(x.device)
+        return torch.zeros(shape).to(x.dtype).cuda()
         # x = x.flatten()
         # in_size = x.shape[0]
         # out_size = math.prod(shape)
