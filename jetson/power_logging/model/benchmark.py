6d053318da7c16cfda621508232ffed974f32841
Misha Iakovlev <misha@fuzzylabs.ai>
2025-01-09 17:31:41 +0000

    Enable benchmarking correctly

--- jetson/power_logging/model/benchmark.py
+++ jetson/power_logging/model/benchmark.py
@@ -146,7 +146,7 @@ def benchmark(args: argparse.Namespace) -> None:
 
             # Records traces in milliseconds
             # https://docs.nvidia.com/deeplearning/tensorrt/api/python_api/infer/Core/Profiler.html#tensorrt.Profiler
-            mods = list(model.named_children())[0]
+            mods = list(model.named_children())
             for mod in mods:
                 if isinstance(mod[1], torch_tensorrt.dynamo.runtime.PythonTorchTensorRTModule):
                     mod[1].enable_profiling(profiler=CustomProfiler())
