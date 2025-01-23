313099d63ae389a1873066c52ede44171014a936
osw282 <25309418+osw282@users.noreply.github.com>
2025-01-23 15:04:29 +0000

    Update working torch model repo version

--- jetson/power_logging/model/model_utils.py
+++ jetson/power_logging/model/model_utils.py
@@ -1,6 +1,6 @@
 import torch
 from typing import Any
-# from model.lenet import LeNet
+from model.lenet import LeNet
 
 def load_model(model_name: str, model_repo: str) -> Any:
     """Load model from Pytorch Hub.
@@ -15,8 +15,8 @@ def load_model(model_name: str, model_repo: str) -> Any:
     Returns:
         PyTorch model
     """
-    # if model_name == "lenet":
-    #     return LeNet()
+    if model_name == "lenet":
+        return LeNet()
     if model_name == "fcn_resnet50":
         return torch.hub.load(model_repo, model_name, pretrained=True)
     try:
@@ -27,10 +27,6 @@ def load_model(model_name: str, model_repo: str) -> Any:
             "Please refer https://pytorch.org/hub/ to get model name."
         )
 
-print(torch.__version__)
-entrypoints = torch.hub.list("pytorch/vision:v0.10.0", force_reload=True)
-print(entrypoints)
-model = load_model("convnext_base", "pytorch/vision:v0.10.0")
     
 def get_layers(model: torch.nn.Module, name_prefix: str="") -> list[tuple[str, torch.nn.Module]]:
     """
