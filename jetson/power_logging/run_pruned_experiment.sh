dea04f48b6ffdb9f1fcb0e60478c483190e9ad84
osw282 <25309418+osw282@users.noreply.github.com>
2025-01-29 14:40:21 +0000

    fix

--- jetson/power_logging/run_pruned_experiment.sh
+++ jetson/power_logging/run_pruned_experiment.sh
@@ -6,7 +6,7 @@ set -eou pipefail
 IDLE_DURATION=120
 
 # Directory to store results
-# RESULT_DIR="raw_data_pruned/prebuilt_models"
+RESULT_DIR="raw_data_pruned/prebuilt_models"
 
 # echo "Running idling power measurement..."
 # python measure_idling_power.py \
