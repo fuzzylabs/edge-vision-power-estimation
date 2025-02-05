85688c4a1cbeeae9dd25c7ad6399c9efb966c3c0
Misha Iakovlev <misha@fuzzylabs.ai>
2025-02-05 09:57:56 +0000

    Add knowledge distillation experiment script

---
+++ jetson/power_logging/run_distillation_experiment.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -eou pipefail
+
+# Time (in seconds) to measure power in idle state
+IDLE_DURATION=120
+
+# Directory to store results
+RESULT_DIR="raw_data/distilled_models"
+
+echo "Running idling power measurement..."
+python measure_idling_power.py \
+  --idle-duration $IDLE_DURATION \
+  --result-dir "$RESULT_DIR"
+
+# Wait for 2 minutes
+echo "Sleeping for 2 minutes..."
+sleep 120
+
+# Models to benchmark
+# Using all YOLOv5 variants
+models=("yolov5nu-from-v5su.pt")
+
+# Iterate through models and run measure_inference_power.py script
+for model in "${models[@]}"
+do
+  echo "Running inference power measurement for model: $model"
+
+  # Run the measure_inference_power.py script
+  python measure_inference_power.py \
+    --model "$model" \
+    --dataset-name "coco.yaml" \
+    --result-dir "$RESULT_DIR"
+done
+
+echo "Experiment completed!"
