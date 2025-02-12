82fa3db6c9f36e4f431105256d3a4b170176d14e
dudeperf3ct <dudeperf3ct@users.noreply.github.com>
2025-02-12 13:20:06 +0000

    Bug: fix model name in experiment script

--- jetson/power_logging/run_experiment.sh
+++ jetson/power_logging/run_experiment.sh
@@ -18,7 +18,7 @@ echo "Sleeping for 2 minutes..."
 sleep 120
 
 # Models to benchmark
-models=("conv_k1_s1_o3" "conv_k2_s1_o3" "single_conv_k3_s1_o3" "conv_k4_s1_o3" "conv_k5_s1_o3")
+models=("conv_k1_s1_o3" "conv_k2_s1_o3" "conv_k3_s1_o3" "conv_k4_s1_o3" "conv_k5_s1_o3")
 # Number of inference cycles
 RUNS=3000
 
